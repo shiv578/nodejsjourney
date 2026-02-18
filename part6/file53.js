dda275cf5ee1dab7800ab46d264ee35e5dee299e
shiv578 <swarajrkfl22@gmail.com>
2026-02-19 02:01:00 +0530

    updated part6

---
+++ part6/file53.js
@@ -0,0 +1,13 @@
+
+//closure
+function outer() {
+  let n = 1;
+  function inner() {
+    return n++;
+  }
+  return inner;
+}
+const counter = outer()
+console.log(counter())
+console.log(counter())
+console.log(counter())
---
+++ part6/file60.js
@@ -0,0 +1,10 @@
+
+const cart = {
+  items: [],
+  addItems: "",
+  showItems: "",
+};
+
+cart.addItems("Laptop")
+cart.addItems("Desktop")
+cart.showItems()
