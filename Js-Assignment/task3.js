0e58d9e2cdf57bf23adf9b817f019cb9f40dac81
shiv578 <swarajrkfl22@gmail.com>
2026-02-19 02:05:23 +0530

    task3

---
+++ Js-Assignment/task3.js
@@ -0,0 +1,6 @@
+// Maintain transaction history for each account.
+// • Support account types: savings and current.
+// • Savings: minimum balance 1000 and 4% yearly interest.
+// • Current: no minimum balance and no interest.
+// • Implement loan feature with maximum loan = 5 times balance.
+// • Implement bank summary functions: total balance and tot
